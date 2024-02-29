5ac0806c56eca03144f78666e0b483e13dcc7d53
Utkarsh Mishra <128682362+utkarshm793@users.noreply.github.com>
2024-02-29 20:55:52 +0000

    javascript

--- Javascript/Index.js
+++ Javascript/Index.js
@@ -90,8 +90,71 @@ user2.name = "mis";
 // console.log(user1.name);
 // console.log(user2.name);
 
+// ******* STRINGS ********
+
+const name1= "utkarsh"
+let surName = "mishra"
+
+// console.log(`my name is ${name1} ${surName}`)
+
+const MyName = new String('adarsh') // it gives like an object a d a r s h 
+
+// console.log(MyName[1])  // it gives (d) of adarsh
+// console.log(MyName.__proto__); // ways to access the strings it gives  = {}
+
+// console.log(MyName.charAt(5)); // it gives h 
+
+// console.log(MyName.indexOf(`r`)); // it gives 3
+
+const newString = `Hello_World`
+
+// console.log(newString.replace('llo', 'bbo'));
+// console.log(newString.includes('he')) // check availability
 
 
+// ******** NUMBERS *********
 
+
+const balance = 234;
+
+// console.log(balance.toString()); // it will convert number data type to string data type
+
+// console.log(balance.toFixed(2)); // it will give 234.00
+
+const number3 = 100000000;
+
+// console.log(number3.toLocaleString());
+
+// ******** MATH LIBRARY *********
+
+
+// console.log(Math.abs(-34)); // gives positive value
+// console.log(Math.round(3.8)); //
+// to get the upper value we should use ceil instead of round and to get the lower value we should use floor
+
+// console.log(Math.floor(Math.random()*10) + 1)
+
+const max = 20;
+const min = 10;
+
+// console.log(Math.floor(Math.random()*(max-min +1)) + min);
+
+// ********* DATE AND TIME *********
+
+let myDate = new Date()
+// console.log(myDate.toString());
+
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let createADate = new Date(2024, 8, 7)
+console.log(createADate.toDateString())
+
+let myTimeStamp = Date.now();
+
+console.log(createADate.getTime());
 
 
